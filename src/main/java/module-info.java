8b54cbdf42e752563fe2ce5c5490ee86947b2a76
Frechsack <61121564+Frechsack@users.noreply.github.com>
2020-08-14 23:36:25 +0200

    Added Version 11 comp.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module frechsack.common.fx
 {
-    requires javafx.graphics;
-    requires javafx.controls;
+    requires transitive javafx.graphics;
+    requires transitive javafx.controls;
     exports com.frechsack.dev.common.fx.container;
     exports com.frechsack.dev.common.fx.geometry;
     exports com.frechsack.dev.common.fx.wirl;
