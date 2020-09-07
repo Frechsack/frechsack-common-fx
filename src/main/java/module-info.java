a748f56274fa81b8ff0ba3ea048266db2030b816
Frechsack <61121564+Frechsack@users.noreply.github.com>
2020-09-07 17:10:56 +0200

    Updated modules.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,5 +5,5 @@ module frechsack.common.fx
     exports com.frechsack.dev.common.fx.container;
     exports com.frechsack.dev.common.fx.geometry;
     exports com.frechsack.dev.common.fx.wirl;
-    exports com;
+    exports com.frechsack.dev.common.fx.stain;
 }
